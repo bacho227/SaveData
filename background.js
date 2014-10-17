f92643227d45347824416d1e95dff62b7cd01d32
bacho <bacho.fifia@gmail.com>
2014-10-17 17:57:43 +0400

    created StoreManager

--- background.js
+++ background.js
@@ -1,11 +1,106 @@
 /**
  * Created by bacho on 10/15/14.
  */
-//console.log("Running at global scope")
-//chrome.browserAction.onClicked.addListener(function (tab) {
-//	console.log(tab)
-//	chrome.tabs.getSelected(null, function (tab) {
-//		var code = "document.querySelector('#deoppcknbhdojlkeincepaocjlgihimo').querySelector('.reload-link').click()";
-//		chrome.tabs.executeScript(tab.id, {code: code});
-//	});
-//});
+function log() {
+	console.log.apply(console, arguments);
+}
+chrome.contextMenus.create({
+	title: "Search: asasds",
+	contexts: ["link", "image"],
+	onclick: log
+}, log);
+
+chrome.extension.onMessage.addListener(function (request, sender, sendResponse) {
+	log(arguments)
+});
+
+function StorageManager() {
+	var me = this;
+	me.data = [];
+
+	me.getData = getData;
+	me.addItem = addItem;
+	me.removeItem = removeItem;
+
+	function getData(callback) {
+		chrome.storage.sync.get(function (res) {
+			me.data = stringToData(objToStr(res));
+			if ($.isFunction(callback)) {
+				callback.call(null, me.data);
+			}
+		});
+	}
+
+	function addItem(item, callback) {
+		me.data.push(item);
+		saveData(callback);
+	}
+
+	function removeItem(item, callback) {
+		me.data.splice(me.data.indexOf(item), 1);
+		saveData(callback);
+	}
+
+	function saveData(callback) {
+		chrome.storage.sync.clear();
+		var data = strToObj(dataToString(me.data));
+		chrome.storage.sync.set(data, checkError(callback));
+	}
+
+	function checkError(callback) {
+		return function () {
+			if (chrome.runtime.lastError) {
+				log(chrome.runtime.lastError);
+				return;
+			}
+
+			if ($.isFunction(callback)) {
+				callback.call(null, me.data);
+			}
+		}
+	}
+
+	function dataToString(data) {
+		var d = [];
+		for (var i in data) {
+			var str = data[i]['url'];
+			if (data[i]['description']) {
+				str += ':' + data[i]['description'];
+			}
+			d.push(str);
+		}
+		return d.join('|');
+	}
+
+	function stringToData(str) {
+		var d = [];
+		var arr = str.split('|');
+		for (var i in arr) {
+			var r = arr[i].split(':');
+			d.push({
+				url: r[0],
+				description: r[1] || null
+			});
+		}
+		return d;
+	}
+
+	function strToObj(str) {
+		var regExp = new RegExp('.{1,' + (chrome.storage.sync.QUOTA_BYTES_PER_ITEM - 50) + '}', 'g');
+		var arr = str.match(regExp);
+		var obj = {};
+		for (var i in arr) {
+			obj[i] = arr[i];
+		}
+		return obj;
+	}
+
+	function objToStr(obj) {
+		var str = '';
+		for (var i in obj) {
+			str += obj[i];
+		}
+		return str;
+	}
+}
+
--- popup.js
+++ popup.js
@@ -4,31 +4,32 @@
 function log() {
 	console.log.apply(console, arguments);
 }
-var data = [
-	{
-		url: 'http://www.jose-aguilar.com/blog/bootstrap-modal/',
-		description: 'asasdasd'
-	},
-	{
-		url: 'http://www.jose-aguilar.com/blog/bootstrap-modal/',
-		description: 'dasasdasd'
-	},
-	{
-		url: 'http://www.jose-aguilar.com/blog/bootstrap-modal/',
-		description: 'asdasd'
-	},
-	{
-		url: 'http://www.jose-aguilar.com/blog/bootstrap-modal/',
-		description: null
-	}
-];
+
+//var stm = new StorageManager();
 
 $(function () {
-	var stm = new StorageManager();
-	stm.getData(function (data) {
-		loadData(data);
+	$('#add-item').on({
+		click: addItem
 	});
+	//	stm.getData(init);
 });
+function init(data) {
+	loadData(data);
+	$('#add-item').on({
+		click: addItem
+	});
+}
+
+function addItem() {
+	log('asds')
+	chrome.extension.sendMessage({type: "getUrls"}, function (response) {
+		log(response)
+	});
+	//	stm.addItem({
+	//		url: escape(unescape("http://getbootstrap.com/components/#input-groups")),
+	//		description: escape($('#new-item-text').val())
+	//	}, loadData);
+}
 
 function loadData(data) {
 	$('.list-group.my-list').empty();
@@ -41,80 +42,10 @@ function getNewItemByObj(obj) {
 	var url = unescape(obj['url']);
 	var item = $('<div class="list-group-item row">' + '<img class="col-xs-1" src="' + url + '"/>' + '<div class="col-xs-10">' + (obj['description'] || url) + '</div>' +
 					 '<span class="col-xs-1 glyphicon glyphicon-remove remove" ></span></div>');
-	item.obj = obj;
+	item.find('.remove').on({
+		click: function () {
+			stm.removeItem(obj, loadData);
+		}
+	});
 	return item;
 }
-
-function StorageManager() {
-	var me = this;
-
-	me.getData = getData;
-	me.setData = setData;
-
-	function getData(callback) {
-		chrome.storage.sync.get(function (res) {
-			var data = stringToData(objToStr(res));
-			callback.call(null, data);
-		});
-	}
-
-	function setData(data, callback) {
-		chrome.storage.sync.clear();
-		var data = strToObj(dataToString(data));
-		chrome.storage.sync.set(data, checkError(callback));
-	}
-
-	function checkError(callbackFn) {
-		return function () {
-			if (chrome.runtime.lastError) {
-				log(chrome.runtime.lastError);
-				return;
-			}
-			callbackFn.apply(null, arguments);
-		}
-	}
-
-	function dataToString(data) {
-		var d = [];
-		for (var i in data) {
-			var str = data[i]['url'];
-			if (data[i]['description']) {
-				str += ':' + data[i]['description'];
-			}
-			d.push(str);
-		}
-		return d.join('|');
-	}
-
-	function stringToData(str) {
-		var d = [];
-		var arr = str.split('|');
-		for (var i in arr) {
-			var r = arr[i].split(':');
-			d.push({
-				url: r[0],
-				description: r[1] || null
-			});
-		}
-		return d;
-	}
-
-	function strToObj(str) {
-		var regExp = new RegExp('.{1,' + (chrome.storage.sync.QUOTA_BYTES_PER_ITEM - 50) + '}', 'g');
-		var arr = str.match(regExp);
-		var obj = {};
-		for (var i in arr) {
-			obj[i] = arr[i];
-		}
-		return obj;
-	}
-
-	function objToStr(obj) {
-		var str = '';
-		for (var i in obj) {
-			str += obj[i];
-		}
-		return str;
-	}
-}
-
